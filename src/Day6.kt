bf97fa155d3ab0ea3575005c52e66b59a3455328
George Wang <George.Wang@ehi.com>
2018-12-06 21:22:27 -0600

    Completed Day 6.

--- src/Day6.kt
+++ src/Day6.kt
@@ -1,10 +1,14 @@
 fun main(args: Array<String>) {  // https://adventofcode.com/2018/day/6#part2
-    part6_1()
+//    part6_1()
     part6_2()
 }
 
 fun part6_2() {
-// Algorithm weakness -- the infinite detection isn't perfect.  The largest result was not the right answer.  Instead, went down to second largest
+    val MAX_DIST = 10000  // reset to 10000 for input
+    val INVALID_DIST = MAX_DIST * 2
+    val buffer = 25  // number of points i 50, so we don't need to look past 25 in each direction
+
+
     val points: List<Pair<Int, Int>> = readAsStringList("Day6.txt", "\n").map { it -> val l = it.replace(" ", "").split(",")
         Pair(l[0].toInt(), l[1].toInt()) }
 
@@ -13,65 +17,47 @@ fun part6_2() {
     val minY = points.minBy { it -> it.second }!!.second
     val maxY = points.maxBy { it -> it.second }!!.second
 
-    val xLen = maxX - minX + 1
-    val yLen = maxY - minY + 1
+    val xLen = maxX - minX + 1 + buffer
+    val yLen = maxY - minY + 1 + buffer
 
-    var grid = List(xLen) { List(yLen) { HashMap<Int, MutableList<Int>>() } }  // each grid: distance, list of points that belong there
+    var grid = List(xLen) { MutableList(yLen) { INVALID_DIST } }  // each grid: distance, list of points that belong there
 
     val stdPoints = points.map { it -> Pair(it.first - minX, it.second - minY) }
 
-    for ((pointName, p) in stdPoints.withIndex()) { // Lesson: withIndex to simplify things
-        for (x in 0 until xLen ) {
-            for (y in 0 until yLen) {
-                val currentPoint = Pair(x, y)
-                // calculate distance to p, add it to the map at the grid
+
+    for (x in 0 until xLen ) {
+        for (y in 0 until yLen) {
+            val currentPoint = Pair(x, y)
+            for ((pointName, p) in stdPoints.withIndex()) { // Lesson: withIndex to simplify things
+                // calculate distance to p, add it
                 val dist = manhattanDist(currentPoint, p)
-                if (grid[x][y].keys.contains(dist)) {
-                    grid[x][y][dist]!!.add(pointName)
-                }
-                else {
-                    grid[x][y].put(dist, MutableList(1){pointName})
+                if (grid[x][y] == INVALID_DIST) {
+                    grid[x][y] = dist
+                } else {
+                    grid[x][y] = grid[x][y] + dist
                 }
             }
         }
     }
+
 
     // get the minimum for each grid
-    var minGrid = List(xLen) { MutableList(yLen) { -1 } }
+//    var minGrid = List(xLen) { MutableList(yLen) { 0 } }
+    var sum = 0
     for (x in 0 until xLen) {
         for (y in 0 until yLen) {
-            val minDist = grid[x][y].minBy { it.key }
-            if (minDist!!.value.size == 1) { // only set it, if there's no tie
-                minGrid[x][y] = minDist!!.value.first()
+            if (grid[x][y] < MAX_DIST) {
+                sum++
+//                minGrid[x][y] = 1
             }
         }
     }
 
-    // finally, get the count of items
-    var pointToArea = HashMap<Int, Int>()
-
-    minGrid.forEach{row ->
-        row.forEach { closestPoint ->
-            if (closestPoint != -1) {
-                if (pointToArea.keys.contains(closestPoint)) {
-                    pointToArea[closestPoint] = pointToArea[closestPoint]!! + 1
-                } else {
-                    pointToArea.put(closestPoint, 1)
-                }
-            }
-        }
-    }
-
-
-    // Debug -- draw the graphs
 //    minGrid.forEach{
 //        println(it)
 //    }
 
-    println("Points to Area: " + pointToArea)
-    println("Areas Sorted: " + pointToArea.values.sorted())
-    println("The max point with area is: " + pointToArea.maxBy{ it.value })
-    println("The max area is: " + pointToArea.values.max()) // 6628 is too high
+    println("Less than maxDist: " + sum)
 }
 
 fun part6_1() {
